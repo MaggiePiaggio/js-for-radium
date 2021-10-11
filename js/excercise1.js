847d517dd7aa4e87084be5dfe5e568d87bf2916c
Magdalena Piaggio <magdalena.piaggio@gmail.com>
2021-10-11 19:18:03 -0300

    js first excercise

---
+++ js/excercise1.js
@@ -0,0 +1,17 @@
+var n1 = 4;
+var n2 = 6;
+console.log (n1+n2);
+var r1 = 'n1 + n2';
+
+var nombre = 'Magdalena';
+var apellido = 'Piaggio';
+var joined = nombre + apellido;
+joined;
+console.log (nombre + apellido);
+
+var phone = 'Motorola';
+var computer = 'Laptop';
+console.log('Motorola is ' + phone.length + ' code units long');
+console.log('The Laptop string has a length of ' + computer.length);
+var tech = phone.length + computer.length;
+console.log (tech);
